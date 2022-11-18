69428b3259e5191c179e605a9b563e3f7339b8cf
jair-mata <101293547+jair-mata@users.noreply.github.com>
2022-11-17 21:19:17 -0600

    Add files via upload

--- server/views/domains/home/homeRouter.js
+++ server/views/domains/home/homeRouter.js
@@ -4,3 +4,9 @@ const addRoutes = (app) {
 
 };
 export default { addRoutes };
+router.get(['/', '/home', '/index'], homeController.home);
+router.get(['/about'], homeController.about);
+const about = (req, res) => {
+    res.render('home/about', {});
+};
+export default { home, about };
